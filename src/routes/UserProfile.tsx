b647434ec7d512079e1d6a9a53fb244946674663
Jo <joely.rooke@gmail.com>
2022-02-07 11:04:37 +0000

    separated modals into divs

--- src/routes/UserProfile.tsx
+++ src/routes/UserProfile.tsx
@@ -46,48 +46,88 @@ export default function UserProfile(props: {
         <em>Alerts all your saved contacts that you are safe</em>
       </p>
       <br />
-      <AllTrips
-        user={props.user}
-        individualTrip={props.individualTrip}
-        setIndividualTrip={props.setIndividualTrip}
-      />
-      <br />
-      <AllContacts user={props.user} />
-      <div
-        className="modal fade"
-        id="addContact"
-        tab-index="-1"
-        role="dialog"
-        aria-labelledby="addContactLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="addContactLabel">
-                Add a Contact
-              </h5>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
+      <div className="tripsComponent">
+        <AllTrips
+          user={props.user}
+          individualTrip={props.individualTrip}
+          setIndividualTrip={props.setIndividualTrip}
+        />
+        <div
+          className="modal fade"
+          id="addTrip"
+          tab-index="-1"
+          aria-labelledby="addTripLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="addTripLabel">
+                  Add a trip
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">Body here</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Cancel
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
             </div>
-            <div className="modal-body">Modal body here</div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                Cancel
-              </button>
-              <button type="button" className="btn btn-primary">
-                Submit
-              </button>
+          </div>
+        </div>
+        <br />
+      </div>
+      <div className="contactsComponent">
+        <AllContacts user={props.user} />
+        <div
+          className="modal fade"
+          id="addContact"
+          tab-index="-1"
+          role="dialog"
+          aria-labelledby="addContactLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="addContactLabel">
+                  Add a Contact
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">Modal body here</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Cancel
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
             </div>
           </div>
         </div>
