0b0514d82ca39d597a35cb537be85adcb2813bb7
Jo <joely.rooke@gmail.com>
2022-02-08 10:36:11 +0000

    debugged adding contact

--- src/utils/handleAddContact.ts
+++ src/utils/handleAddContact.ts
@@ -7,16 +7,27 @@ export default function handleAddContact(
   name: string,
   email: string,
   userId: number,
-  setState: React.Dispatch<React.SetStateAction<IContact[]>>
+  setState: React.Dispatch<React.SetStateAction<IContact[]>>,
+  setName: React.Dispatch<React.SetStateAction<string>>,
+  setEmail: React.Dispatch<React.SetStateAction<string>>
 ): void {
+  function resetStates(
+    setName: React.Dispatch<React.SetStateAction<string>>,
+    setEmail: React.Dispatch<React.SetStateAction<string>>
+  ) {
+    setName("");
+    setEmail("");
+  }
+
   axios
-    .put(baseUrl + `/contacts/${userId}`, {
+    .post(baseUrl + `/contacts/${userId}`, {
       contactName: name,
       contactEmail: email,
     })
     .then(() => {
       getData(baseUrl, setState);
     })
+    .then(() => resetStates(setName, setEmail))
     .catch((error) => {
       console.log(error);
     });
