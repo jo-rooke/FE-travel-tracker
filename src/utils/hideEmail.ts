c4adab8b0de2f86c5653609d54ddea7668d197e4
Jo <joely.rooke@gmail.com>
2022-02-08 10:35:47 +0000

    hid email accounting for short email ids

--- src/utils/hideEmail.ts
+++ src/utils/hideEmail.ts
@@ -1,13 +1,19 @@
 export default function hideEmail(fullEmail: string): string {
-  const splitEmail = fullEmail.split("@"); //
-  const first6 = fullEmail.slice(0, 6);
-  const starredOut = splitEmail[0].slice(6);
+  const splitEmail = fullEmail.split("@");
+  let visible;
+  let starredOut;
+  if (splitEmail[0].length > 5) {
+    visible = splitEmail[0].slice(0, -5);
+    starredOut = splitEmail[0].slice(-5);
+  } else {
+    visible = splitEmail[0].slice(0, 1);
+    starredOut = splitEmail[0].slice(1);
+  }
   const stars = [];
   for (let letter of starredOut) {
     letter = "*";
     stars.push(letter);
   }
   const tail = `@${splitEmail[1]}`;
-
-  return `${first6}${stars.join("")}${tail}`;
+  return `${visible}${stars.join("")}${tail}`;
 }
