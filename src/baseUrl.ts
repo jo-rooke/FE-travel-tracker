aceecdeb448e445c8dfd77d1070b48557507d64f
Jo <joely.rooke@gmail.com>
2022-02-23 12:34:07 +0000

    changed baseurl

--- src/baseUrl.ts
+++ src/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://jr-travel-tracker.herokuapp.com";
-export const baseUrl = "http://localhost:4000";
+export const baseUrl = "https://jr-travel-tracker.herokuapp.com";
+// export const baseUrl = "http://localhost:4000";
